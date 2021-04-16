b94ab6e264c1b5c25264add68972f0d84865dd1d
shimanekb <brandon.j.shimanek@gmail.com>
2021-04-16 06:30:47 -0500

    input file made

--- create_input.sh
+++ create_input.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 INPUT_FILE_NAME=$1
-INPUT_NUMBER=$2
+INPUT_NUMBER=3000000
 
 
 echo "type,key1,key2,value" >> $INPUT_FILE_NAME
-for (( n=0; n<$INPUT_NUMBER; n++ ))
+for (( n=2000000; n<$INPUT_NUMBER; n++ ))
 do
   key=$(printf "key%013d" $n)
   value=$(printf "val%013d" $n)
